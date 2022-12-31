1473f0d425aa27a93f8da7001936c57873a344a1
David G Rosenberg <targendaz2@me.com>
2023-01-01 03:35:43 +0800

    Initial commit

---
+++ card_manager/tests/test_functional.py
@@ -0,0 +1,8 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+class FunctionalTests(StaticLiveServerTestCase):
+
+    pass
