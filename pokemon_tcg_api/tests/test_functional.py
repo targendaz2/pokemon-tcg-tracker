e371ee43bb070ebca42d111d901c72172ba90bd5
David G Rosenberg <targendaz2@me.com>
2023-01-02 15:49:54 +0800

    1st functional test passes

--- pokemon_tcg_api/tests/test_functional.py
+++ pokemon_tcg_api/tests/test_functional.py
@@ -1,6 +1,8 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 pokemon_names = [
     "Bulbasaur",
@@ -47,22 +49,55 @@ pokemon_names = [
 
 class FunctionalTests(StaticLiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.chrome_options = Options()
+        cls.chrome_options.headless = True
+        cls.selenium = WebDriver(options=cls.chrome_options)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
     def test_search_for_existing_cards(self):
         # A user navigates to the website.
+        self.selenium.get(self.live_server_url)
 
-        # They're presented with a search bar.
-
-        # They enter a Pokémon's name and click "search".
+        # They enter a Pokémon's name into the search bar and click "search".
+        search_bar = self.selenium.find_element(
+            By.CSS_SELECTOR, 'input[type="text"]')
+        search_bar.send_keys("Pikachu")
+        search_button = self.selenium.find_element(
+            By.CSS_SELECTOR, 'input[value="Search"]')
+        search_button.click()
 
         # They're presented with the number of cards containing that name...
+        results_count = self.selenium.find_element(
+            By.XPATH, "//*[contains(text(),'Results: ')]")
 
         # ...as well as the first few cards containing that name.
+        results = self.selenium.find_elements(
+            By.XPATH, "//*[contains(text(),'Pikachu')]")
+        self.assertGreater(len(results), 0)
 
         # The user enters another Pokémon's name and clicks "search".
+        search_bar = self.selenium.find_element(
+            By.CSS_SELECTOR, 'input[type="text"]')
+        search_bar.send_keys("Porygon")
+        search_button = self.selenium.find_element(
+            By.CSS_SELECTOR, 'input[value="Search"]')
+        search_button.click()
 
         # They're presented with the number of cards containing that name...
+        results_count = self.selenium.find_element(
+            By.XPATH, "//*[contains(text(),'Results: ')]")
 
         # ...as well as the first few cards containing that name.
+        results = self.selenium.find_elements(
+            By.XPATH, "//*[contains(text(),'Porygon')]")
+        self.assertGreater(len(results), 0)
 
         # Satisfied, the user leaves the site.
-        self.fail()
