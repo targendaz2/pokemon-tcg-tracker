38ae862aed5d6bd6cec04afb66bfd8be2cf53d77
David G Rosenberg <targendaz2@me.com>
2023-01-03 11:08:40 +0800

    Unit test passing

--- pokemon_tcg_api/tests/test_unit.py
+++ pokemon_tcg_api/tests/test_unit.py
@@ -1,17 +1,63 @@
-from django.http import HttpRequest
-from django.test import TestCase
-from django.urls import resolve
+from django.test import TestCase, Client
+from django.urls import reverse
+
+import random
 
 from .. import views
+
+pokemon_names = [
+    "Bulbasaur",
+    "Ivysaur",
+    "Venusaur",
+    "Charmander",
+    "Charmeleon",
+    "Charizard",
+    "Squirtle",
+    "Wartortle",
+    "Blastoise",
+    "Pikachu",
+    "Raichu",
+    "Ditto",
+    "Eevee",
+    "Vaporeon",
+    "Jolteon",
+    "Flareon",
+    "Porygon",
+    "Articuno",
+    "Zapdos",
+    "Moltres",
+    "Mewtwo",
+    "Chikorita",
+    "Bayleef",
+    "Meganium",
+    "Cyndaquil",
+    "Quilava",
+    "Typhlosion",
+    "Totodile",
+    "Croconaw",
+    "Feraligatr",
+    "Pichu",
+    "Espeon",
+    "Umbreon",
+    "Porygon2",
+    "Raiku",
+    "Entei",
+    "Suicune",
+    "Lugia",
+    "Ho-Oh"
+]
 
 class ViewTests(TestCase):
     
     def test_index_includes_name_from_POST(self):
-        # Generate the request
+        client = Client()
+        name = random.choice(pokemon_names)
+
+        # Generate the POST data
+        post_data = {'name': name}
 
         # Submit it to the index view
+        response = client.post(reverse('index'), post_data)
 
-        # Get the output
-
-        # Check for the name from the POST data
-        self.fail()
+        # Check the response for the card name
+        self.assertContains(response, name)
--- pokemon_tcg_api/views.py
+++ pokemon_tcg_api/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        return render(request, 'index.html', {'name': name})
     return render(request, 'index.html')
