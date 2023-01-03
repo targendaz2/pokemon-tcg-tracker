6c14abfa7ef5a1afccea2eac2afd29059a9cb90f
David G Rosenberg <targendaz2@me.com>
2023-01-03 10:51:06 +0800

    1st unittest is failing

--- pokemon_tcg_api/tests/test_unit.py
+++ pokemon_tcg_api/tests/test_unit.py
@@ -1,1 +1,17 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from django.urls import resolve
+
+from .. import views
+
+class ViewTests(TestCase):
+    
+    def test_index_includes_name_from_POST(self):
+        # Generate the request
+
+        # Submit it to the index view
+
+        # Get the output
+
+        # Check for the name from the POST data
+        self.fail()
