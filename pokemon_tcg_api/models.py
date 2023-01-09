e9b5a55353b473360c27bec47d6f1e5e596eea85
David G Rosenberg <targendaz2@me.com>
2023-01-09 17:37:52 +0800

    Added str method to Card model

--- pokemon_tcg_api/models.py
+++ pokemon_tcg_api/models.py
@@ -5,3 +5,6 @@ class Card(models.Model):
 
     id = models.CharField(max_length=24, primary_key=True)
     name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
